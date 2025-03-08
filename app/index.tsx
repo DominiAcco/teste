dce697c5db0e991dde521b27d455a85d38690595
DominiAcco <dominiacco@gmail.com>
2025-03-07 21:43:14 -0400

    criaçao da tesk e lista de tesk

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,15 @@
 
+import { useState } from "react";
 import { Button, Text, View , StyleSheet, TextInput, TouchableOpacity} from "react-native";
 
 export default function Index() {
+  const [task, setTask] = useState('')
+  const [myTasks, setMyTasks]= useState([''])
 
+  function handleAddTask(){
+    setMyTasks(oldState =>[...oldState, task])
+    setTask('')
+  }
  
 
 
@@ -18,16 +25,43 @@ export default function Index() {
       placeholder="Compra Pão"
       placeholderTextColor='#555'
       style={style.input}
+      value = {task}
+      onChangeText={setTask}
      
      /> 
 
      <TouchableOpacity
       activeOpacity={0.5}
       style={style.button}
-     
+      onPress={handleAddTask}
      >
       <Text style={style.buttonText}>Adicionar</Text>
+      
+     
+     
      </TouchableOpacity>
+
+
+
+    <Text style={[style.title, {marginVertical:30 }]}>A fazeres</Text>
+    {myTasks.map( (item, index) => (
+      <TouchableOpacity 
+        key={index}
+        style={style.buttonTask}>
+          <Text style={style.textTask}>
+        
+            {item}
+          </Text>
+      </TouchableOpacity>
+     ))}
+     
+      
+    
+
+
+     
+
+     
     </View>
   );
 }
@@ -63,5 +97,17 @@ const style = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     fontSize: 17
+  },
+  buttonTask:{
+    backgroundColor:'#1F1E25',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  textTask:{
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold'
   }
 })
